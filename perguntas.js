b829d004b9d09e06b5e4d0a8b62c06592a594fef
balabanzinha <146238675+gabrielafreiberger@users.noreply.github.com>
2024-11-12 09:55:50 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Bíblia',
+    'Bíblia 01',
     'Qual é o livro mais curto da Bíblia?',
     'O livro mais curto da Bíblia é Obadias, que possui apenas 21 versículos.'
 )
 
 criaCartao(
-    'Bíblia',
+    'Bíblia 02',
     'Quem escreveu a maioria dos Salmos?',
     'A maioria dos Salmos é atribuída ao rei Davi, embora outros autores também tenham contribuído.'
 )
 
 criaCartao(
-    'Bíblia',
+    'Bíblia 03',
     'Qual é o nome do primeiro homem criado segundo a Bíblia?',
     'O primeiro homem criado segundo a Bíblia é Adão.'
 )
 
 criaCartao(
-    'Bíblia',
+    'Bíblia 04',
     'Qual é o principal ensinamento do Sermão da Montanha?',
     'O Sermão da Montanha, encontrado em Mateus 5-7, ensina sobre a ética do Reino de Deus, incluindo as bem-aventuranças e a importância do amor e da misericórdia.'
 )
