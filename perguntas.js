c5f5d4500491392d83d51cbb8e680f510e02b8fc
balabanzinha <146238675+gabrielafreiberger@users.noreply.github.com>
2024-11-12 09:46:54 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Paris'
 )
 
 criaCartao(
@@ -19,5 +19,5 @@ criaCartao(
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Oi em ingles é hi'
 )
