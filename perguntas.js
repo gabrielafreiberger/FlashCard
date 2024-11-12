3ea34d9ca784ba4d383d4839b2dc2366f74cf49f
balabanzinha <146238675+gabrielafreiberger@users.noreply.github.com>
2024-11-12 09:51:58 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'Uma linguagem de programação'
+    'Bíblia',
+    'Qual é o livro mais curto da Bíblia?',
+    'O livro mais curto da Bíblia é Obadias, que possui apenas 21 versículos.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'Paris'
+    'Bíblia',
+    'Quem escreveu a maioria dos Salmos?',
+    'A maioria dos Salmos é atribuída ao rei Davi, embora outros autores também tenham contribuído.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Bíblia',
+    'Qual é o nome do primeiro homem criado segundo a Bíblia?',
+    'O primeiro homem criado segundo a Bíblia é Adão.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é hi'
+    'Bíblia',
+    'Qual é o principal ensinamento do Sermão da Montanha?',
+    'O Sermão da Montanha, encontrado em Mateus 5-7, ensina sobre a ética do Reino de Deus, incluindo as bem-aventuranças e a importância do amor e da misericórdia.'
 )
